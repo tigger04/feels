38a33f42c720cc310de038e4440290fd4826c14d
tigger <tigger04@gmail.com>
2024-03-05 09:14:31 +0000

    Initial Commit

---
+++ feels/feelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  feelsApp.swift
+//  feels
+//
+//  Created by tigger on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct feelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
